3507f136486e4608cd395012cea984c041e56d0b
Charles Zhang <charleszhangmb@gmail.com>
2022-04-15 00:09:40 -0700

    Its a mess

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 // NextJS
 import Head from "next/head";
+import Image from "next/image";
 
 // Stylesheet
 import styles from "styles/Home.module.scss";
@@ -13,9 +14,25 @@ const Home: NextPage = () => {
 			<Head>
 				<title>Victoria Delk's Blog</title>
 			</Head>
-            <div id={styles.home}>
-                
-            </div>
+			<div id={styles.home}>
+				<p id={styles["home-text"]}>Hi!</p>
+				<div id={styles["home-collage"]}>
+					<div id={styles["img-1"]} className={styles["home-img"]}>
+						<Image
+							src="/profile1.JPG"
+							layout="fill"
+							objectFit="cover"
+						/>
+					</div>
+					<div id={styles["img-2"]} className={styles["home-img"]}>
+						<Image
+							src="/profile2.JPG"
+							layout="fill"
+							objectFit="cover"
+						/>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
