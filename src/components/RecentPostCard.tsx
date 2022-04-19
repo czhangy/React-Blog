9c4cf86667a6fc4be4ec971c0cc6233e327a9864
Charles Zhang <charleszhangmb@gmail.com>
2022-04-19 16:17:11 -0700

    homepage skeleton

--- src/components/RecentPostCard.tsx
+++ src/components/RecentPostCard.tsx
@@ -1,17 +1,53 @@
+// NextJS
+import Link from "next/link";
+import Image from "next/image";
+
 // Stylesheet
-import styles from "styles/Home.module.scss";
+import styles from "styles/RecentPostCard.module.scss";
 
 type Props = {
 	post: {
+        id: String,
 		title: String;
+		timestamp: String;
+		thumb: string;
+		body: String;
 	};
 };
 
 const RecentPostCard = ({ post }: Props) => {
+	// Truncate body text and insert ellipses
+	const truncateBody = () => {
+		if (post.body.length > 175) return post.body.substring(0, 175) + "...";
+		else return post.body;
+	};
+
 	return (
-		<ul className={styles["recent-post-card"]}>
-			<h4>{post.title}</h4>
-		</ul>
+		<li className={styles["recent-post-card"]}>
+			<Link href={"/blog/" + post.id}>
+				<a className={styles["card-container"]}>
+					<div className={styles["image-container"]}>
+						<Image
+							src={post.thumb}
+							alt=""
+							height={160}
+							width={120}
+						/>
+					</div>
+					<div className={styles["content-container"]}>
+						<div className={styles["header-container"]}>
+							<h4 className={styles["post-title"]}>
+								{post.title}
+							</h4>
+							<p className={styles["post-timestamp"]}>
+								{post.timestamp}
+							</p>
+						</div>
+						<p className={styles["post-body"]}>{truncateBody()}</p>
+					</div>
+				</a>
+			</Link>
+		</li>
 	);
 };
 export default RecentPostCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 // NextJS
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 // Stylesheet
 import styles from "styles/Home.module.scss";
@@ -8,21 +9,48 @@ import styles from "styles/Home.module.scss";
 // TS
 import type { NextPage } from "next";
 
+// Components
+import RecentPostCard from "components/RecentPostCard";
+
 const Home: NextPage = () => {
+	const test = {
+		id: "test",
+		title: "Test Post #1",
+		timestamp: new Date().toISOString().slice(0, 10),
+		thumb: "/profile1.JPG",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non justo in ex interdum vestibulum. Morbi vulputate ligula quis pharetra mattis. Sed magna ligula, rutrum vel porttitor eu, interdum nec ex. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus eu lacus fermentum, tincidunt ligula eget, aliquet nulla. Nulla nec malesuada justo. Ut eget hendrerit ipsum. Pellentesque pretium eros vel purus ullamcorper lobortis. Maecenas sit amet gravida libero. Aenean in mi mi. Nam consectetur elementum dui, ut sollicitudin sem pellentesque nec. Cras risus ipsum, interdum dapibus ullamcorper id, posuere at est. Interdum et malesuada fames.",
+	};
+
 	return (
-		<div>
+		<>
 			<Head>
 				<title>Victoria Delk&apos;s Blog</title>
 			</Head>
 			<div id={styles.home}>
-				<div id={styles["image-wrapper"]}>
-					<Image src="/profile1.JPG" alt="" height="400" width="300" />
+				<div id={styles["image-container"]}>
+					<div id={styles["image-wrapper"]}>
+						<Image
+							src="/profile1.JPG"
+							alt=""
+							height="600"
+							width="450"
+						/>
+					</div>
+					<p id={styles["main-title"]}>Hi! I'm Vicky Delk :)</p>
 				</div>
 				<div id={styles["recent-posts-container"]}>
-					<h2>RECENT POSTS</h2>
+					<h2 id={styles["recent-posts-header"]}>RECENT POSTS</h2>
+					<ul id={styles["recent-posts-list"]}>
+						<RecentPostCard post={test} />
+						<RecentPostCard post={test} />
+						<RecentPostCard post={test} />
+					</ul>
+					<Link href="/blog">
+						<a id={styles["see-more-link"]}>See More!</a>
+					</Link>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
